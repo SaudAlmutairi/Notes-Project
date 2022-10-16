b6af013420bb6ac4ace49a6ae0435c631dd603b3
SaudAlmutiai <saud701ms@gmail.com>
2022-10-17 00:25:52 +0300

    add Scroll Back To Top Button

--- app.js
+++ app.js
@@ -156,3 +156,21 @@ function displayNotes(notes){
 async function clearAll(){
     let request = await NotesObj.clear();
 }
+
+let mybutton =document.getElementById("Btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || 
+    document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    } else{
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
